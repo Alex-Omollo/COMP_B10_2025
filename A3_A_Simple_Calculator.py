1ecef6d65c37121af29ea9d5edddd2785b976fc4
Blackgover <71961064+Alex-Omollo@users.noreply.github.com>
2025-04-23 19:19:47 +0300

    A simple compund intrest calculator

---
+++ A3_A_Simple_Calculator.py
@@ -0,0 +1,18 @@
+# Welcome the user
+print("#" * 60)
+print("Welcome to a simple compound intrest calculator".center(60))
+print("#" * 60)
+print("\n")
+
+# Get user input
+flPrinciple = float(input("What is the principle amount: "))
+flRate = float(input("What is the intrest rate: "))
+flTime = float(input("What is the time taken: "))
+flCompound = float(input("How many times is it compounded: "))
+
+# Calculating the intrest
+flAmount = flPrinciple * (1 + (flRate / 100) / flCompound) ** (flCompound * flTime)
+
+# Final answer
+print(f"The intrest rate for a principal of {flPrinciple} and a time of {flTime} with an intrest rate of {flRate} "
+      f"compunded at {flCompound} gives a total amount of {flAmount}")
